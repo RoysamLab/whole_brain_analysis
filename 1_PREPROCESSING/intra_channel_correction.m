bf4fab0855d136cfd610bfe34eb44a9998d5925d
jjahanip <jahandar.jahani@gmail.com>
2019-08-08 23:59:56 -0400

    add brightfield to intra_channel

--- 1_PREPROCESSING/intra_channel_correction.m
+++ 1_PREPROCESSING/intra_channel_correction.m
@@ -1,4 +1,4 @@
-function out = intra_channel_correction(input_dir, output_dir, disk_size)
+function out = intra_channel_correction(input_dir, output_dir, disk_size, brightfield)
 tic
 
 % check if output directory exis
@@ -8,25 +8,30 @@ end
 
 image_fnames = dir(fullfile(input_dir, '*.tif'));
 for i=1:size(image_fnames, 1)
+    
     % read image and get the histogram of the original image
     t_in = Tiff(fullfile(input_dir, image_fnames(i).name), 'r+');
     im = read(t_in);
     close(t_in);
-    % calculate the background using morphological opening
-    background = im;
-    for j = 1:length(disk_size)
-        % create disk for morphological opening and closing
-        se = strel('disk',disk_size(j));
-        % Alternative Sequential filter
-        background = imopen(background,se);
+    
+    % if this is not the brightfield channel
+    if ~contains(image_fnames(i).name, int2str(brightfield))
+        % calculate the background using morphological opening
+        background = im;
+        for j = 1:length(disk_size)
+            % create disk for morphological opening and closing
+            se = strel('disk',disk_size(j));
+            % Alternative Sequential filter
+            background = imopen(background,se);
+        end
+
+        % extract the background from original image
+        im = im - background;
+
+        % normalize to 0-65535
+        im = double(im);
+        im = uint16(im - min(im(:)))*(65535 / (max(im(:)) - min(im(:))));
     end
-    
-    % extract the background from original image
-    im = im - background;
-    
-    % normalize to 0-65535
-    im = double(im);
-    im = uint16(im - min(im(:)))*(65535 / (max(im(:)) - min(im(:))));
     
     % write image to disk
     write_bigtiff(im, fullfile(output_dir, image_fnames(i).name));
