4d3bd5d3712260802b65e3e86d613c62b5c7daae
jjahanip <jahandar.jahani@gmail.com>
2019-09-05 16:25:51 -0400

    back to 1.Registration 2.Intra / log to h/m/s

--- image_processing_unsupervised.py
+++ image_processing_unsupervised.py
@@ -7,23 +7,12 @@ import subprocess
 # NAMING PROTOCOL: RxCy (x = round number | y = channel number)
 ###################################################################
 
-INPUT_DIR = r'E:\jahandar\DashData\G3_BR#15_HC_12L\original'
-OUTPUT_DIR = r'E:\jahandar\DashData\G3_BR#15_HC_12L'
+INPUT_DIR = r'E:\jahandar\DashData\TBI\G3_BR#14_HC_11L\original'
+OUTPUT_DIR = r'E:\jahandar\DashData\TBI\G3_BR#14_HC_11L\unsupervised'
 BRIGHTFIELD = 11
 
-# INTRA-CHANNEL CORRECTION
-input_dir = INPUT_DIR
-output_dir = os.path.join(OUTPUT_DIR, 'IL_corrected')
-disk_size = [20, 40]
-command = ' '.join(["matlab -nojvm -nosplash -nodesktop -wait -r",
-                    "\"addpath(fullfile(pwd, '1_PREPROCESSING'));",
-                    "intra_channel_correction('{}','{}',{}, {}); quit\"".format(input_dir, output_dir, disk_size, BRIGHTFIELD)])
-start = time.time()
-p = subprocess.call(command, shell=True)
-print('Intra-channel fluorescence correction pipeline finished successfully in {:.2f} seconds.'.format(time.time() - start))
-
 # REGISTRATION
-input_dir = os.path.join(OUTPUT_DIR, 'IL_corrected')
+input_dir = INPUT_DIR
 output_dir = os.path.join(OUTPUT_DIR, 'registered')
 command = ' '.join([r"python 1_PREPROCESSING/registration.py",
                    "--input_dir={}".format(input_dir),
@@ -35,10 +24,27 @@ command = ' '.join([r"python 1_PREPROCESSING/registration.py",
                    "-nk 30000"])
 start = time.time()
 p = subprocess.call(command, shell=True)
-print('Registration pipeline finished successfully in {:.2f} seconds.'.format(time.time() - start))
+duration = time.time() - start
+m, s = divmod(int(duration), 60)
+h, m = divmod(m, 60)
+print('Registration pipeline finished successfully in {:d} hours, {:d} minutes and {:d} seconds.'.format(h, m, s))
+
+# INTRA-CHANNEL CORRECTION
+input_dir = os.path.join(OUTPUT_DIR, 'registered')
+output_dir = os.path.join(OUTPUT_DIR, 'IL_corrected')
+disk_size = [20, 40]
+command = ' '.join(["matlab -nojvm -nosplash -nodesktop -wait -r",
+                    "\"addpath(fullfile(pwd, '1_PREPROCESSING'));",
+                    "intra_channel_correction('{}','{}',{}, {}); quit\"".format(input_dir, output_dir, disk_size, BRIGHTFIELD)])
+start = time.time()
+p = subprocess.call(command, shell=True)
+duration = time.time() - start
+m, s = divmod(int(duration), 60)
+h, m = divmod(m, 60)
+print('Intra-channel fluorescence correction pipeline finished successfully in {:d} hours, {:d} minutes and {:d} seconds.'.format(h, m, s))
 
 # INTER-CHANNEL CORRECTION UNSUPERVISED
-input_dir = os.path.join(OUTPUT_DIR, 'registered')
+input_dir = os.path.join(OUTPUT_DIR, 'IL_corrected')
 output_dir = os.path.join(OUTPUT_DIR, 'unmixed')
 command = ' '.join(["python 1_PREPROCESSING/inter_channel_correction_unsupervised.py",
                     "--input_dir={}".format(input_dir),
@@ -46,4 +52,7 @@ command = ' '.join(["python 1_PREPROCESSING/inter_channel_correction_unsupervise
                      "--brightfield={}".format(BRIGHTFIELD)])
 start = time.time()
 p = subprocess.call(command, shell=True)
-print('Inter-channel fluorescence correction pipeline finished successfully in {:.2f} seconds.'.format(time.time() - start))
+duration = time.time() - start
+m, s = divmod(int(duration), 60)
+h, m = divmod(m, 60)
+print('Inter-channel fluorescence correction pipeline finished successfully in {:d} hours, {:d} minutes and {:d} seconds.'.format(h, m, s))
