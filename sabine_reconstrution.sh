0964f9dc10a350245f7a655f54e92a31efa92682
--global <jahandar.jahani@gmail.com>
2019-10-15 10:53:11 -0500

    update sabine sbatch file

--- sabine_reconstrution.sh
+++ sabine_reconstrution.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#SBATCH -J G4_BR_19
-#SBATCH -o G4_BR_19
+#SBATCH -J G2_BR_22
+#SBATCH -o G2_BR_22
 #SBATCH -t 24:00:00
 #SBATCH -N 1 -n 28
 #SBATCH --mem 240gb
 #SBATCH -A roysam
 
+module load Anaconda3
 source activate /brazos/roysam/shared/miniconda/envs/brain
 
-python main_reconstruction_linux.py
-
+python main_reconstruction_lin.py
