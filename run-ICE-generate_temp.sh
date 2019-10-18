33e7b4b6ed54a4432b86e7b73931046828670d8e
Xiaoyang-Rebecca <xiaoyang.rebecca.li@gmail.com>
2019-10-18 11:53:44 -0500

    sabine_generate_ICE

---
+++ run-ICE-generate_temp.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#SBATCH -p medium --mem=125GB --exclusive
+#SBATCH -t 50:00:00 
+#SBATCH -o run-ICE-generate_temp.outfile
+#SBATCH -e run-ICE-generate_temp.errfile
+
+cd /brazos/roysam/xli63/exps/SegmentationPipeline/DataAnalysis
+
+#pip install fcswrite --user
+data_dir="/brazos/roysam/50_plex/Set#1_S1"
+python GenerateICE_FCS_script.py \
+--inputDir="$data_dir"/final \
+--maskType=b \
+--maskDir="$data_dir"/detection_results/bbxs_detection.txt \
+--outputDir="$data_dir"/ICE_FCS_files_temp \
+--saveVis=1 \
+--downscaleRate=4 \
+--seedSize=3
