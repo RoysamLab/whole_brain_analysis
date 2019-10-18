fab5b4757d6fc7030c49fc0700ab733f89a50c0c
Xiaoyang-Rebecca <xiaoyang.rebecca.li@gmail.com>
2019-10-18 11:56:02 -0500

    add xmldir

--- sabine_generateICE.sh
+++ sabine_generateICE.sh
@@ -6,13 +6,16 @@
 
 cd /brazos/roysam/xli63/exps/SegmentationPipeline/DataAnalysis
 
+# Gerate ICE [From bbox]  
+
 #pip install fcswrite --user
 data_dir="/brazos/roysam/50_plex/Set#1_S1"
 python GenerateICE_FCS_script.py \
 --inputDir="$data_dir"/final \
---maskType=b \
+--maskType=b \   # bbbox
 --maskDir="$data_dir"/detection_results/bbxs_detection.txt \
 --outputDir="$data_dir"/ICE_FCS_files_temp \
+--xmlDir=$xmldir
 --saveVis=1 \
 --downscaleRate=4 \
 --seedSize=3
