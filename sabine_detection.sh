16ed97f5f7e87d74e3d4aceeb12d0f1d8e53739f
jjahanip <jahandar.jahani@gmail.com>
2019-11-04 16:21:55 -0600

    update cuDNN version on sabine_detection.sh

--- sabine_detection.sh
+++ sabine_detection.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH -J G2_BR_22 
-#SBATCH -o G2_BR_22
+#SBATCH -J G2_BR_22_det
+#SBATCH -o G2_BR_22_det
 #SBATCH -t 24:00:00
 #SBATCH -N 1 -n 28
 #SBATCH -p gpu
@@ -10,12 +10,12 @@
 #SBATCH -A roysam
 
 module load CUDA/.10.0.130
-module load cuDNN/7.5.0-CUDA-10.0.130
+module load cuDNN/7.4.2.24-CUDA-10.0.130
 
 module load Anaconda3
 source activate /brazos/roysam/shared/miniconda/envs/brain
 
 python main_detection.py \
-       --INPUT_DIR=/brazos/roysam/datasets/TBI/G2_Sham_Trained/G2_BR#22_HC_13L/original \
+       --INPUT_DIR=/brazos/roysam/datasets/TBI/G2_Sham_Trained/G2_BR#22_HC_13L/detection_results \
        --OUTPUT_DIR=/brazos/roysam/datasets/TBI/G2_Sham_Trained/G2_BR#22_HC_13L/detection_results \
        --DAPI=R2C1.tif \
