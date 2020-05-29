9b67f52c41f86a6b4f889263250d139a4945a66c
jjahanip <jahandar.jahani@gmail.com>
2020-05-29 00:14:57 -0500

    update maui script

---
+++ scripts/slurm/maui/maui_classification.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH -J G4_19
+#SBATCH -o log_class
+#SBATCH -t 24:00:00
+#SBATCH -N 1 -n 5 
+#SBATCH --gres gpu:1
+#SBATCH --mem 50g
+
+module use /project/ece/roysam/jahandar/software/modulefiles/all
+module load CUDA/9.0.176 
+
+source /project/ece/roysam/jahandar/software/miniconda/bin/activate /project/ece/roysam/jahandar/software/miniconda/envs/brain
+
+cd /project/ece/roysam/jahandar/whole_brain_analysis
+
+python main_classification.py \
+       --INPUT_DIR /project/ece/roysam/datasets/TBI/G4_mFPI_Li+VPA/G4_BR#19_HC_12R/final \
+       --OUTPUT_DIR /project/ece/roysam/datasets/TBI/G4_mFPI_Li+VPA/G4_BR#19_HC_12R/classification_results \
+       --BBXS_FILE /project/ece/roysam/datasets/TBI/G4_mFPI_Li+VPA/G4_BR#19_HC_12R/detection_results/bbxs_detection.txt \
+       --DAPI R1C1.tif \
+       --NEUN R1C4.tif \
+       --S100 R2C6.tif \
+       --IBA1 R1C7.tif \
+       --RECA1 R1C6.tif \
+       --test_mode first \
+       --thresholds .5 .5 1 .93 .5
--- scripts/slurm/maui/maui_detection.sh
+++ scripts/slurm/maui/maui_detection.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-#SBATCH -J G2_BR_22_det
-#SBATCH -o G2_BR_22_det
+#SBATCH -J G4_19
+#SBATCH -o log_det
 #SBATCH -t 24:00:00
-#SBATCH -N 1 -n 1
-#SBATCH -p gpu
+#SBATCH -N 1 -n 5 
 #SBATCH --gres gpu:1
 #SBATCH --mem 50g
 
+module use /project/ece/roysam/jahandar/software/modulefiles/all
 module load CUDA/9.0.176 
 
-module load Anaconda3
-source activate /project/hnguyen/jahandar/software/miniconda/envs/brain
+source /project/ece/roysam/jahandar/software/miniconda/bin/activate /project/ece/roysam/jahandar/software/miniconda/envs/brain
+
+cd /project/ece/roysam/jahandar/whole_brain_analysis
 
 python main_detection.py \
-       --INPUT_DIR=/project/hnguyen/datasets/TBI/G3_mFPI_Vehicle/G3_BR#18_HC_14L/detection_results \
-       --OUTPUT_DIR=/project/hnguyen/datasets/TBI/G3_mFPI_Vehicle/G3_BR#18_HC_14L/detection_results \
+       --INPUT_DIR=/project/ece/roysam/datasets/TBI/G3_mFPI_Vehicle/G3_BR#18_HC_14L/detection_results \
+       --OUTPUT_DIR=/project/ece/roysam/datasets/TBI/G3_mFPI_Vehicle/G3_BR#18_HC_14L/detection_results \
        --DAPI=R2C1.tif \
